8836cf17d3bde24af6b4d97ff10e91aa14307329
Supt <1012388@sal.ipg.pt>
2018-05-26 16:48:23 +0100

    Adicionei a possibilidade de criar treinos.

--- app/src/main/java/pt/ipg/gestortreinos/MainActivity.java
+++ app/src/main/java/pt/ipg/gestortreinos/MainActivity.java
@@ -8,25 +8,58 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements OnClickListener{
+    LinearLayout parent;
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+       // createNewButton();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+       fab.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                  //      .setAction("Action", null).show();
+                createNewButton();
+
+
+
+
+
             }
         });
     }
+
+    private void createNewButton() {
+        //Create a new button. The button will intent to new activity.
+        //That ativity will be to create a new training report
+        String[]button_names = {"Treino 1","Treino 2","Treino 3"};
+        LinearLayout parent = (LinearLayout) findViewById(R.id.l1_parent);
+        int id = 0;
+        int tag = id;
+            b1 = new Button (MainActivity.this);
+            b1.setId(id);
+            b1.setText(button_names[id+1]);
+            b1.setTag(tag);
+            parent.addView(b1);
+            b1.setOnClickListener(MainActivity.this);
+            id++;
+            //Toast.makeText(getApplicationContext(),""+id,Toast.LENGTH_SHORT ).show();
+            //Erro com o height do linearlayout:Não aparece os primeiros butões
+
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -45,8 +78,23 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        }
+        }//Adicionar aqui opções de eliminar treinos,alterar nome do treino,
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String tag = v.getTag().toString();
+
+        if(tag.equals("0")){
+            Toast.makeText(getApplicationContext(),"Treino 1",Toast.LENGTH_SHORT ).show();
+
+        }else if(tag.equals("1")){
+            Toast.makeText(getApplicationContext(),"Treino 2",Toast.LENGTH_SHORT ).show();
+        }else if(tag.equals("2")){
+            Toast.makeText(getApplicationContext(),"Treino 3",Toast.LENGTH_SHORT ).show();
+        }
+
     }
 }
--- app/src/main/java/pt/ipg/gestortreinos/Treinos.java
+++ app/src/main/java/pt/ipg/gestortreinos/Treinos.java
@@ -1,4 +1,23 @@
 package pt.ipg.gestortreinos;
 
 public class Treinos {
+  /*  Treino
+		-Duração
+		-Peso usado
+		-Exercico feito
+		-Dia do treino
+		-Repetições
+
+    Dias de semana:
+            -Grupo muscular(Costas,Pernas,Peito,Braços)
+			-Tipo de dia(Treino ou Descanso)
+			-Dia*/
+
+  //Classe Treinos, vai ser referente à atividade onde o utilizador vai poder escrever o seu treino, num certo dia
+
+/*Main activity vai ser a primeira atividade que o utilizador vê.
+* A primeira coisa que o utilizador vai ver,vai ser os seus treinos,organizados por data de criação(O mais recente é o primeiro)
+*
+* */
+
 }
