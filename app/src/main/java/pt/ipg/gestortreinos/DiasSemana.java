7494976f1e9335f6f12d2f90d1fd0154f8871939
Supt <1012388@sal.ipg.pt>
2018-06-15 12:33:55 +0100

    Criação da função "getAndroidSystemDay"

--- app/src/main/java/pt/ipg/gestortreinos/DiasSemana.java
+++ app/src/main/java/pt/ipg/gestortreinos/DiasSemana.java
@@ -1,13 +1,17 @@
 package pt.ipg.gestortreinos;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class DiasSemana {
 
     private int idDia;
-
     private String nome_dia;
+    private int current_day = 0;
 
 
     public int getIdDia() {
+        idDia = getAndroidSystemDay();
         return idDia;
     }
 
@@ -23,4 +27,34 @@ public class DiasSemana {
         this.nome_dia = nome_dia;
     }
 
+
+    public int getAndroidSystemDay() {
+        Date current_date = null;
+
+        int segunda = Calendar.MONDAY;
+        int terca = Calendar.TUESDAY;
+        int quarta = Calendar.WEDNESDAY;
+        int quinta = Calendar.THURSDAY;
+        int sexta = Calendar.FRIDAY;
+
+        switch (current_date.getDay()) {
+            case 2:
+                current_day = segunda;
+                break;
+            case 3:
+                current_day = terca;
+                break;
+            case 4:
+                current_day = quarta;
+                break;
+            case 5:
+                current_day = quinta;
+                break;
+            case 6:
+                current_day = sexta;
+                break;
+        }
+
+        return current_day;
+    }
 }
